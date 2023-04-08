e96d0b9ebc74c18f60409da588fdd267d8466678
EZ4ZZW <zhiwayzhang@outlook.com>
2023-04-09 01:34:17 +0800

    refactor: analysis.py && add legend

--- analysis.py
+++ analysis.py
@@ -4,20 +4,23 @@ from more_itertools import chunked
 
 data = pd.read_csv('data.csv')
 
+Smothly = True
+Smothly_Interval = 5
+
 time = data['time']
 tt_util = data['total_util']
 gc_util = data['gc_util']
 
 # smothly handle the data
-tt_util = [sum(x)/len(x) for x in chunked(tt_util, 5)]
-gc_util = [sum(x)/len(x) for x in chunked(gc_util, 5)]
+if Smothly:
+    tt_util = [sum(x)/len(x) for x in chunked(tt_util, Smothly_Interval)]
+    gc_util = [sum(x)/len(x) for x in chunked(gc_util, Smothly_Interval)]
+    length = len(tt_util)
+    time = time[:length]
+    for i in range(0, len(time)):
+        time[i] *= Smothly_Interval
 
-length = len(tt_util)
-time = time[:length]
-for i in range(0, len(time)):
-    time[i]*=5
-
-plt.plot(time, tt_util, color='blue')
-plt.plot(time, gc_util, color='red')
-
+plt.plot(time, tt_util, color='blue', label='total_util')
+plt.plot(time, gc_util, color='red', label='gc_util')
+plt.legend()
 plt.show()
