33d26070023b29fe844b264cbf920dd4acb5bc91
EZ4ZZW <zhiwayzhang@outlook.com>
2023-04-25 16:43:53 +0800

    update: refactor dd_test.sh with function

--- Experiments/dd/dd_test.sh
+++ Experiments/dd/dd_test.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-start_time=$(date +%s)
+StartDD() {
+    dd if=/dev/zero of=/tmp/test/$1 bs=$2 count=$3 oflag=direct 2>&1 > ./$1.log
+	local end_time=$(date +%s)
+	echo "End Time of $1 is $end_time"
+	local cost=$((end_time-start_time))
+	echo "Total cost of $1 is $cost seconds."
+}
 
-dd if=/dev/zero of=/tmp/test/file1 bs=1M count=1800 oflag=direct &
+start_time=$(date +%s)
+echo "Start At $start_time"
+StartDD dd.file1 1M 1800 &
 pid1=$!
 echo $pid1
 
-dd if=/dev/zero of=/tmp/test/file2 bs=1M count=1800 oflag=direct &
+StartDD dd.file2 1M 900 &
 pid2=$!
 echo $pid2
 
 wait $pid1 $pid2
-
-end_time=$(date +%s)
-cost=$((end_time-start_time))
-echo "Total cost in $cost seconds."
+echo "Test Complete"
