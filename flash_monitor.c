25dbf9e15c0e54f490bafa4c9d49ee3689bfa8f9
EZ4ZZW <zhiwayzhang@outlook.com>
2023-04-30 14:35:31 +0800

    tidy: format code

--- flash_monitor.c
+++ flash_monitor.c
@@ -42,15 +42,15 @@ static void bwset_nl_recv_msg(struct sk_buff *skb)
 	msg_size = sizeof(data);
 	printk(KERN_INFO "netlink_test: Received from pid %d, size: %d\n", pid, msg_size);
 	mutex_lock(&ci_mutex);
-    buffer[buffer_length].pid = data[0];
+	buffer[buffer_length].pid = data[0];
 	buffer[buffer_length].tag = data[1];
 	buffer_length += 1;
 	mutex_unlock(&ci_mutex);
-    printk(KERN_INFO "Received integers: %d, %d\n", data[0], data[1]);
+	printk(KERN_INFO "Received integers: %d, %d\n", data[0], data[1]);
 }
 
 static struct netlink_kernel_cfg nl_cfg = {
-    .input = bwset_nl_recv_msg,
+	.input = bwset_nl_recv_msg,
 };
 
 // create cgroup
@@ -70,7 +70,7 @@ static struct netlink_kernel_cfg nl_cfg = {
 // remove {pid} {tag/cgroup}
 // remove pid from cgroup
 
-int bw_regulator_main(void *data) 
+int bw_regulator_main(void *data)
 {
 	// kernel sys calls
 	long (*do_rmdir)(int dfd, const char __user *pathname);
@@ -86,11 +86,13 @@ int bw_regulator_main(void *data)
 	ksys_write = (void *)kallsyms_lookup_name("ksys_write");
 	// init cgroup fs
 	int err = do_mkdirat(-100, "/sys/fs/cgroup/front", 0755);
-	if (err < 0) {
+	if (err < 0)
+	{
 		printk("mk /sys/fs/cgroup/front Failed !\n");
 	}
 	err = do_mkdirat(-100, "/sys/fs/cgroup/back", 0755);
-	if (err < 0) {
+	if (err < 0)
+	{
 		printk("mk /sys/fs/cgroup/back Failed !\n");
 	}
 	// Flag for Status
@@ -104,14 +106,16 @@ int bw_regulator_main(void *data)
 		total_util = shared_total_util_info;
 		gc_util = shared_gc_util_info;
 		read_unlock(&lock);
-		int d_total_util = total_util*MOD;
-		int d_gc_util = gc_util*MOD;
+		int d_total_util = total_util * MOD;
+		int d_gc_util = gc_util * MOD;
 		printk("REGULATOR: in last 5s : total=%d gc=%d\n", d_total_util, d_gc_util);
-		if (1 - total_util < UTIL_THRESHOLD && !THRESHOLD_ENABLED) {
+		if (1 - total_util < UTIL_THRESHOLD && !THRESHOLD_ENABLED)
+		{
 			// do threshold
 			THRESHOLD_ENABLED = true;
 		}
-		if (1 - total_util > UTIL_THRESHOLD && THRESHOLD_ENABLED) {
+		if (1 - total_util > UTIL_THRESHOLD && THRESHOLD_ENABLED)
+		{
 			// disable threshold
 			THRESHOLD_ENABLED = false;
 		}
@@ -119,11 +123,13 @@ int bw_regulator_main(void *data)
 	}
 	// remove cgroup fs
 	err = do_rmdir(-100, "/sys/fs/cgroup/front");
-	if (err < 0) {
+	if (err < 0)
+	{
 		printk("rmdir /sys/fs/cgroup/front Failed !\n");
 	}
 	err = do_rmdir(-100, "/sys/fs/cgroup/back");
-	if (err < 0) {
+	if (err < 0)
+	{
 		printk("rmdir /sys/fs/cgroup/back Failed !\n");
 	}
 	// clear pointer
@@ -190,24 +196,25 @@ int flash_monitor_main(void *data)
 		printk("counter = %d, result = %u \n", counter, le32_to_cpu(cmd.result));
 		result = le32_to_cpu(cmd.result);
 
-		gc_util += 1.0*(result % MOD)/MOD;
-		total_util += 1.0*result/MOD/MOD;
+		gc_util += 1.0 * (result % MOD) / MOD;
+		total_util += 1.0 * result / MOD / MOD;
 		// process threshold
-		if (counter == SAMPLING_TIMES) {
+		if (counter == SAMPLING_TIMES)
+		{
 			gc_util /= SAMPLING_TIMES;
 			total_util /= SAMPLING_TIMES;
-			int d_total_util = total_util*MOD;
-			int d_gc_util = gc_util*MOD;
+			int d_total_util = total_util * MOD;
+			int d_gc_util = gc_util * MOD;
 			printk("MONITOR: in last 5s : total=%d gc=%d\n", d_total_util, d_gc_util);
 			write_lock(&lock);
 			shared_total_util_info = total_util;
-        	shared_gc_util_info = gc_util;
-        	write_unlock(&lock);
+			shared_gc_util_info = gc_util;
+			write_unlock(&lock);
 			counter = 0;
 			gc_util = 0;
 			total_util = 0;
 		}
- 		ssleep(SAMPLING_INTERVAL);
+		ssleep(SAMPLING_INTERVAL);
 	}
 	// close fd
 	int res = ksys_close(fd);
@@ -232,15 +239,16 @@ int init_monitor(void)
 
 	buffer_length = 0;
 	// create netlink socket
-    nl_sk = netlink_kernel_create(&init_net, NETLINK_USER, &nl_cfg);
-    if (!nl_sk) {
-        printk(KERN_ERR "Failed to create netlink socket\n");
-        return -ENOMEM;
-    }
+	nl_sk = netlink_kernel_create(&init_net, NETLINK_USER, &nl_cfg);
+	if (!nl_sk)
+	{
+		printk(KERN_ERR "Failed to create netlink socket\n");
+		return -ENOMEM;
+	}
 
 	shared_gc_util_info = 0;
 	shared_total_util_info = 0;
-	
+
 	flash_monitor = kthread_create(flash_monitor_main, NULL, "nand_flash_util");
 	if ((flash_monitor))
 	{
@@ -277,9 +285,10 @@ void monitor_exit(void)
 	}
 	// rwlock_destroy(&lock);
 	printk("release lock ! \n");
-	if (nl_sk) {
-        netlink_kernel_release(nl_sk);
-    }
+	if (nl_sk)
+	{
+		netlink_kernel_release(nl_sk);
+	}
 	printk("release netlink socket ! \n");
 }
 
