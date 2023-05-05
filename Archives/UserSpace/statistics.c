6394792ddd907b49c04e38495256713e21674f75
EZ4ZZW <zhiwayzhang@outlook.com>
2023-05-05 19:07:49 +0800

    feat: ctrl-c signal capturer handle exit

--- Archives/UserSpace/statistics.c
+++ Archives/UserSpace/statistics.c
@@ -4,9 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <signal.h>
 const int MOD = 10000;
 
-int to_csv(double *total_utilization, double *gc_utilization, int length)
+int to_csv(double *total_utilization, double *gc_utilization, int *time_stamp, int length)
 {
 	FILE *fileptr;
 	fileptr = fopen("./data.csv", "w");
@@ -14,33 +15,41 @@ int to_csv(double *total_utilization, double *gc_utilization, int length)
 		printf("file open error\n");
 		return -1;
 	}
-	fprintf(fileptr, "time,total_util,gc_util\n");
+	fprintf(fileptr, "time,total_util,gc_util,time_stamp\n");
 	for (int i = 0; i < length; i++) {
-		fprintf(fileptr, "%d,%lf,%lf\n", i+1, total_utilization[i], gc_utilization[i]);
+		fprintf(fileptr, "%d,%lf,%lf,%d\n", i+1, total_utilization[i], gc_utilization[i], time_stamp[i]);
 	}
 	fclose(fileptr);
 	return 0;
 }
 
-static int do_statistic(int dev_fd, double *total_utilization, double *gc_utilization, 
+static volatile bool should_stop = false;
+
+void signal_handler(int sig)
+{
+	if (sig == SIGINT)
+	{
+		should_stop = true;
+	}
+	return;
+}
+
+static int do_statistic(int dev_fd, double *total_utilization, double *gc_utilization, int *time_stamp,
 					int length, double sampling_interval, double sampling_time)
 {
 	int err;
 	int start_time = time(NULL);
 	double total = 0;
-	clock_t start, end;
+	// clock_t start, end;
 	int writer = 0;
 	unsigned int result = 0;
-	while (true) {
+	while (!should_stop) {
 		if (writer >= length) break;
 		int now_time = time(NULL);
 		if (now_time - start_time < sampling_interval) continue;
 		start_time = now_time;
 		// do nvme admin command submti
-		start = clock();
 		err = nvme_sct_op(dev_fd, 3, 1, 0, 0, 0, &result);
-		end = clock();
-		total += (double)(end - start)/CLOCKS_PER_SEC;
 		if (err < 0) {
 			printf("errno: %d\n", err);
 			return -1;
@@ -49,10 +58,11 @@ static int do_statistic(int dev_fd, double *total_utilization, double *gc_utiliz
 		// get the last 4 digits
 		gc_utilization[writer] = 1.0*(result % MOD)/MOD;
 		total_utilization[writer] = 1.0*result/MOD/MOD;
+		time_stamp[writer] = now_time;
 		if (total_utilization[writer] > 1.0) total_utilization[writer] = 1.0;
 		writer++;
 	}
-	printf("total = %lf ms\n", total*1000);
+	to_csv(total_utilization, gc_utilization, time_stamp, writer);
 	return 0;
 }
 
@@ -70,6 +80,7 @@ static int get_device_fd()
 
 int main()
 {
+	signal(SIGINT, signal_handler);
 	int dev_fd = get_device_fd();
 	if (dev_fd < 0) {
 		return 0;
@@ -77,13 +88,14 @@ int main()
 	// statistic array
 	double *total_utilization = NULL;
 	double *gc_utilization = NULL;
-	double sampling_interval = 1; // seconds
-	double sampling_time = 60; // seconds
+	int *time_stamp;
+	double sampling_interval = 3; // seconds
+	double sampling_time = 3600*24; // seconds
 	int length = sampling_time/sampling_interval;
     total_utilization = malloc(sizeof(double)*length);
     gc_utilization = malloc(sizeof(double)*length);
-	do_statistic(dev_fd, total_utilization, gc_utilization, length, sampling_interval, sampling_time);
+	time_stamp = malloc(sizeof(int)*length);
+	do_statistic(dev_fd, total_utilization, gc_utilization, time_stamp, length, sampling_interval, sampling_time);
 	close(dev_fd);
-	to_csv(total_utilization, gc_utilization, length);
 	return 0;
 }
